5ccc6675e6ef6afe871d9eb67dd83c3564622812
reactmore <reactmorecom@gmail.com>
2022-03-15 19:57:21 +0700

    Test Bot

--- src/Commands/getUpdatesCLI.php
+++ src/Commands/getUpdatesCLI.php
@@ -5,6 +5,10 @@ namespace Reactmore\TelegramBotCore\Commands;
 use Config\Autoload;
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Exception\TelegramLogException;
+use Longman\TelegramBot\TelegramLog;
 
 /**
  * Enables Task Running
@@ -37,7 +41,31 @@ class getUpdatesCLI extends BaseCommand
         $this->getConfig();
 
         CLI::newLine(1);
-        CLI::write($this->config->hook_url . '**** Completed ****', 'white', 'red');
+        CLI::write('**** Running BOT ****', 'white', 'green');
+        $this->bot();
         CLI::newLine(1);
+    }
+
+    protected function bot()
+    {
+        try {
+            $this->getConfig();
+            $telegram = new Telegram($this->config->ApiKey, $this->config->bot_username);
+
+            $server_response = $telegram->handleGetUpdates();
+
+            if ($server_response->isOk()) {
+                $update_count = count($server_response->getResult());
+                echo date('Y-m-d H:i:s') . ' - Processed ' . $update_count . ' updates';
+            } else {
+                echo date('Y-m-d H:i:s') . ' - Failed to fetch updates' . PHP_EOL;
+                echo $server_response->printError();
+            }
+        } catch (TelegramException $e) {
+            TelegramLog::error($e);
+            // echo $e;
+        } catch (TelegramLogException $e) {
+            // echo $e;
+        }
     }
 }
