ea32dd59360747c04a67c30168ef81c2c070243b
reactmore <reactmorecom@gmail.com>
2022-03-17 21:19:38 +0700

    update

---
+++ src/Controllers/Webhook.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Reactmore\TelegramBotCore\Controllers;
+
+class Webhook extends TelegramController
+{
+    public function index()
+    {
+        echo 'Index';
+    }
+
+    //maintenance mode
+    public function setwebhook()
+    {
+        echo 'Index';
+    }
+}
